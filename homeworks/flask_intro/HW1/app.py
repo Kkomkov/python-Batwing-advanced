9662fcb51412a12f0db421a2ff10b0138dd4002c
Kostiantyn Komkov <Kkomkov99@gmail.com>
2022-06-27 13:45:05 +0100

    implement flask into hw_1

---
+++ homeworks/flask_intro/HW1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hi():    
+    return "Hi man"
+
+@app.route("/sum/<int:x1>/<int:x2>", methods=['GET'])
+def sum(x1,x2):    
+    return x1+x2
+
+if __name__ == "__main__":
+    app.run(port=5000,  host='0.0.0.0', debug=True)
