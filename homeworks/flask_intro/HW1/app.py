712875aa6110a67565db0946a5687d89df53cade
Kostiantyn Komkov <Kkomkov99@gmail.com>
2022-06-29 13:41:44 +0100

    fix api response

--- homeworks/flask_intro/HW1/app.py
+++ homeworks/flask_intro/HW1/app.py
@@ -8,7 +8,7 @@ def hi():
 
 @app.route("/sum/<int:x1>/<int:x2>", methods=['GET'])
 def sum(x1,x2):    
-    return x1+x2
+    return str(x1+x2)
 
 if __name__ == "__main__":
     app.run(port=5000,  host='0.0.0.0', debug=True)
